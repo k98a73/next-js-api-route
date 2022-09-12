9c0745df7005ad5d6c7c802a4aa1643c3b32fb55
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-12 16:50:49 +0900

    クライアントでの設定

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ export default function Home() {
   const [weather, setWeather] = useState("");
   useEffect(() => {
     const fetchWeather = async () => {
-      const response = await fetch("/api/weather");
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&lang=ja`
+      );
       const data = await response.json();
       setWeather(data);
     };
