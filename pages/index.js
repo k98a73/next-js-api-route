8866b7a7e0f9fc209b6515f79ff65e157f5c57f9
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-09-12 16:57:23 +0900

    getServerSideProps関数での設定

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,12 @@
-import { useState, useEffect } from "react";
-
-export default function Home() {
-  const [weather, setWeather] = useState("");
-  useEffect(() => {
-    const fetchWeather = async () => {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}&lang=ja`
-      );
-      const data = await response.json();
-      setWeather(data);
-    };
-    fetchWeather();
-  }, []);
-
-  return (
-    <div>{weather.weather && <p>東京の天気：{weather.weather[0].main}</p>}</div>
+export async function getServerSideProps() {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${process.env.OPEN_WEATHER_API_KEY}&lang=ja`
   );
+  const data = await response.json();
+
+  return { props: { data } };
+}
+
+export default function Home({ data }) {
+  return <div>{data.weather && <p>東京の天気：{data.weather[0].main}</p>}</div>;
 }
